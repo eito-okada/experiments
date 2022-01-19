f7812ad582b2ceff57576f503b8e8d158fcb8bbb
eito-okada <eito8okada@gmail.com>
2022-01-19 16:54:45 +0900

    commi

--- script10.js
+++ script10.js
@@ -1,20 +1,41 @@
-$('#load1').load("./index12.html .date, .name", function() {
+$('#load1').load("./index12.html .date, .name, .post", function() {
 
     var data = new Array($('.date').length);
     for (var i = 0; i < $('.date').length; i++) {
-        data[i] = [$('.name')[i].innerHTML, $('.date')[i].innerHTML];
+        data[i] = [$('.name')[i].innerHTML, $('.date')[i].innerHTML, $('.post').eq(i).attr('data-genre')];
     }
 
-    data = data.sort(function(a, b) {
-        return b[1] > a[1] ? 1 : b[1] < a[1] ? -1 : 0;
+    $.getScript('./script11.js', function() {
+        $(document).ajaxStop(function() {
+
+            for (i = 0; i < data.length; i++) {
+                data[i][3] = visitsdata[i]
+            }
+
+            data = data.sort(function(a, b) {
+                return b[1] > a[1] ? 1 : b[1] < a[1] ? -1 : 0;
+            });
+
+            $('#name1').text(data[0][0]);
+            $('#date1').text(data[0][1]);
+            $('#name2').text(data[1][0]);
+            $('#date2').text(data[1][1]);
+            $('#name3').text(data[2][0]);
+            $('#date3').text(data[2][1]);
+
+            data = data.sort(function(a, b) {
+                return b[3] > a[3] ? 1 : b[3] < a[3] ? -1 : 0;
+            })
+
+            $('#name4').text(data[0][0]);
+            $('#date4').text(data[0][1]);
+            $('#visits1').text(data[0][3])
+            $('#name5').text(data[1][0]);
+            $('#date5').text(data[1][1]);
+            $('#visits2').text(data[1][3])
+            $('#name6').text(data[2][0]);
+            $('#date6').text(data[2][1]);
+            $('#visits3').text(data[2][3])
+        })
     });
-
-    $('#name1').text(data[0][0]);
-    $('#date1').text(data[0][1]);
-    $('#name2').text(data[1][0]);
-    $('#date2').text(data[1][1]);
-    $('#name3').text(data[2][0]);
-    $('#date3').text(data[2][1]);
-
-    console.table(data);
 });
--- script11.js
+++ script11.js
@@ -1,178 +1,73 @@
-$(function() {
-    visits121221();
-
-    var visitsdata = new Array($('.post').length);
-
-    function visits121221() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.121221/visits", function(response) {
-            $("#visits121221").text(response.value);
-            visitsdata[0] = response.value;
-        });
-        visits121321();
-    }
-
-
-    function visits121321() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.121321/visits", function(response) {
-            $("#visits121321").text(response.value);
-            visitsdata[1] = response.value;
-        });
-        visits121421();
-    }
-
-
-    function visits121421() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.121421/visits", function(response) {
-            $("#visits121421").text(response.value);
-            visitsdata[2] = response.value;
-        });
-        visits121521();
-    }
-
-
-    function visits121521() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.121521/visits", function(response) {
-            $("#visits121521").text(response.value);
-            visitsdata[3] = response.value;
-        })
-        visits121621();
-    }
-
-
-
-    function visits121621() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.121621/visits", function(response) {
-            $("#visits121621").text(response.value);
-            visitsdata[4] = response.value;
-        });
-        visits121721();
-    }
-
-
-
-    function visits121721() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.121721/visits", function(response) {
-            $("#visits121721").text(response.value);
-            visitsdata[5] = response.value;
-        });
-        visits121821();
-    }
-
-
-
-    function visits121821() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.121821/visits", function(response) {
-            $("#visits121821").text(response.value);
-            visitsdata[6] = response.value;
-        });
-        visits121921();
-    }
-
-
-
-
-    function visits121921() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.121921/visits", function(response) {
-            $("#visits121921").text(response.value);
-            visitsdata[7] = response.value;
-        });
-        visits122021();
-    }
-
-
-
-    function visits122021() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122021/visits", function(response) {
-            $("#visits122021").text(response.value);
-            visitsdata[8] = response.value;
-        });
-        visits122121();
-    }
-
-
-
-    function visits122121() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122121/visits", function(response) {
-            $("#visits122121").text(response.value);
-            visitsdata[9] = response.value;
-        });
-        visits122221();
-    }
-
-
-
-    function visits122221() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122221/visits", function(response) {
-            $("#visits122221").text(response.value);
-            visitsdata[10] = response.value;
-        });
-        visits122321();
-    }
-
-
-
-
-    function visits122321() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122321/visits", function(response) {
-            $("#visits122321").text(response.value);
-            visitsdata[11] = response.value;
-        });
-        visits122421();
-    }
-
-
-    function visits122421() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122421/visits", function(response) {
-            $("#visits122421").text(response.value);
-            visitsdata[12] = response.value;
-        });
-        visits122521();
-    }
-
-
-
-    function visits122521() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122521/visits", function(response) {
-            $("#visits122521").text(response.value);
-            visitsdata[13] = response.value;
-        });
-        visits122621();
-    }
-
-
-
-    function visits122621() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122621/visits", function(response) {
-            $("#visits122621").text(response.value);
-            visitsdata[14] = response.value;
-        });
-        visits122721();
-    }
-
-
-
-    function visits122721() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122721/visits", function(response) {
-            $("#visits122721").text(response.value);
-            visitsdata[15] = response.value;
-        });
-        visits122821();
-    }
-
-
-
-    function visits122821() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122821/visits", function(response) {
-            $("#visits122821").text(response.value);
-            visitsdata[16] = response.value;
-        });
-        visits122921();
-    }
-
-    function visits122921() {
-        $.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122921/visits", function(response) {
-            $('#visits122921').text(response.value);
-            visitsdata[17] = response.value;
-        })
-    }
+var visitsdata = [];
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.121221/visits", function(response1) {
+    $("#visits121221").text(response1.value);
+    visitsdata[0] = response1.value;
+});
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.121321/visits", function(response2) {
+    $("#visits121321").text(response2.value);
+    visitsdata[1] = response2.value;
+});
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.121421/visits", function(response3) {
+    $("#visits121421").text(response3.value);
+    visitsdata[2] = response3.value;
+});
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.121521/visits", function(response4) {
+    $("#visits121521").text(response4.value);
+    visitsdata[3] = response4.value;
+})
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.121621/visits", function(response5) {
+    $("#visits121621").text(response5.value);
+    visitsdata[4] = response5.value;
+});
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.121721/visits", function(response6) {
+    $("#visits121721").text(response6.value);
+    visitsdata[5] = response6.value;
+});
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.121821/visits", function(response7) {
+    $("#visits121821").text(response7.value);
+    visitsdata[6] = response7.value;
+});
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.121921/visits", function(response8) {
+    $("#visits121921").text(response8.value);
+    visitsdata[7] = response8.value;
+});
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122021/visits", function(response9) {
+    $("#visits122021").text(response9.value);
+    visitsdata[8] = response9.value;
+});
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122121/visits", function(response10) {
+    $("#visits122121").text(response10.value);
+    visitsdata[9] = response10.value;
+});
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122221/visits", function(response11) {
+    $("#visits122221").text(response11.value);
+    visitsdata[10] = response11.value;
+});
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122321/visits", function(response12) {
+    $("#visits122321").text(response12.value);
+    visitsdata[11] = response12.value;
+});
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122421/visits", function(response13) {
+    $("#visits122421").text(response13.value);
+    visitsdata[12] = response13.value;
+});
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122521/visits", function(response14) {
+    $("#visits122521").text(response14.value);
+    visitsdata[13] = response14.value;
+});
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122621/visits", function(response15) {
+    $("#visits122621").text(response15.value);
+    visitsdata[14] = response15.value;
+});
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122721/visits", function(response16) {
+    $("#visits122721").text(response16.value);
+    visitsdata[15] = response16.value;
+});
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122821/visits", function(response17) {
+    $("#visits122821").text(response17.value);
+    visitsdata[16] = response17.value;
+});
+$.getJSON("https://api.countapi.xyz/get/hatchokada.lsv.jp.122921/visits", function(response18) {
+    $('#visits122921').text(response18.value);
+    visitsdata[17] = response18.value;
 })
