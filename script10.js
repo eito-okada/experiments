fedef8f785d4ec85c04368c4571b6368f3d7b631
eito-okada <eito8okada@gmail.com>
2022-01-19 22:58:34 +0900

    commit

--- script10.js
+++ script10.js
@@ -1,6 +1,7 @@
 $('#load1').load("./index12.html .date, .name, .post", function() {
 
     var data = new Array($('.date').length);
+
     for (var i = 0; i < $('.date').length; i++) {
         data[i] = [$('.name')[i].innerHTML, $('.date')[i].innerHTML, $('.post').eq(i).attr('data-genre')];
     }
@@ -12,6 +13,7 @@ $('#load1').load("./index12.html .date, .name, .post", function() {
                 data[i][3] = visitsdata[i]
             }
 
+
             data = data.sort(function(a, b) {
                 return b[1] > a[1] ? 1 : b[1] < a[1] ? -1 : 0;
             });
@@ -22,6 +24,7 @@ $('#load1').load("./index12.html .date, .name, .post", function() {
             $('#date2').text(data[1][1]);
             $('#name3').text(data[2][0]);
             $('#date3').text(data[2][1]);
+            console.table(data)
 
             data = data.sort(function(a, b) {
                 return b[3] > a[3] ? 1 : b[3] < a[3] ? -1 : 0;
@@ -36,6 +39,26 @@ $('#load1').load("./index12.html .date, .name, .post", function() {
             $('#name6').text(data[2][0]);
             $('#date6').text(data[2][1]);
             $('#visits3').text(data[2][3])
+            console.table(data)
+
+            for (var i = 0; i < data.length; i++) {
+                if (data[i][2] !== $('#content').attr('data-genre')) {
+                    data.splice(i, 1);
+                    i--;
+                }
+            }
+
+            $('#name7').text(data[0][0]);
+            $('#date7').text(data[0][1]);
+            $('#visits7').text(data[0][3])
+            $('#name8').text(data[1][0]);
+            $('#date8').text(data[1][1]);
+            $('#visits8').text(data[1][3])
+            $('#name9').text(data[2][0]);
+            $('#date9').text(data[2][1]);
+            $('#visits9').text(data[2][3])
+
+            console.table(data)
         })
     });
 });
